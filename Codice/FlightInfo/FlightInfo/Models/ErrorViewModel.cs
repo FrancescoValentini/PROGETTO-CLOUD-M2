4aba6c7f9b461efc9de116f7b033881743c7ef3d
Francesco Valentini <Francesco.valentini930@gmail.com>
2024-12-04 16:46:13 +0100

    Inizializzato progetto MVC Visual Studio

---
+++ Codice/FlightInfo/FlightInfo/Models/ErrorViewModel.cs
@@ -0,0 +1,7 @@
+namespace FlightInfo.Models {
+    public class ErrorViewModel {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
