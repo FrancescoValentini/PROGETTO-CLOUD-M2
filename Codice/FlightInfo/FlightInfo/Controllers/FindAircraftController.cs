2d5c11987bcf2b6d9d866869f4f61f30c6eedd11
GiuliaTrz <148765503+GiuliaTrz@users.noreply.github.com>
2024-12-04 17:29:03 +0100

    Inizializzata la View e FindAircraftController

---
+++ Codice/FlightInfo/FlightInfo/Controllers/FindAircraftController.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FlightInfo.Controllers;
+
+public class FindAircraftController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
