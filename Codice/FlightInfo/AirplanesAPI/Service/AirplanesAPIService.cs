0944fcd43ea849cca33120b35dd9bd40b8b4189a
Giulia Balestra <balestrag9@gmail.com>
2024-12-09 17:31:01 +0100

    Realizzato il metodo DoGETRequest
    Il metodo DoGETRequest effettua una richiesta GET all'API e restituisce il JSON

--- Codice/FlightInfo/AirplanesAPI/Service/AirplanesAPIService.cs
+++ Codice/FlightInfo/AirplanesAPI/Service/AirplanesAPIService.cs
@@ -1,6 +1,37 @@
+using System.Runtime.CompilerServices;
+using static System.Net.WebRequestMethods;
+
 namespace AirplanesAPI.Service;
 
 public class AirplanesAPIService
 {
-    
+    public static async Task<string> DoGETRequest(int Scelta , string ParametroDiRicerca)
+    {
+        string BaseUrl = "https://api.airplanes.live/v2/";
+
+
+        switch(Scelta)
+        {
+            case 1:
+                BaseUrl = BaseUrl + "callsign/";
+
+                break;
+               
+            case 2:
+                BaseUrl = BaseUrl + "hex/";
+
+                break;
+
+        }
+
+        BaseUrl = BaseUrl+ParametroDiRicerca;
+
+        HttpClient Client = new HttpClient();
+        // Esegue una richiesta GET all'API e ottiene la risposta JSON
+        HttpResponseMessage Response = await Client.GetAsync(BaseUrl);
+        Response.EnsureSuccessStatusCode();
+        string json = await Response.Content.ReadAsStringAsync();
+
+       return json;
+    }
 }
