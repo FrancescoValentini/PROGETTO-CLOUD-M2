ebcdaf8644bc760b848c8dc0ec28d06e90969259
Giulia Balestra <balestrag9@gmail.com>
2024-12-09 17:58:08 +0100

    Inizializzato progetto per i Test (APITest)

---
+++ Codice/FlightInfo/APITest/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace APITest
+{
+    public static class APITest
+    {
+         public static void Main(string[] args)
+        {
+            // Display the number of command line arguments.
+            Console.WriteLine("Hello world");
+        }
+    }
+}
