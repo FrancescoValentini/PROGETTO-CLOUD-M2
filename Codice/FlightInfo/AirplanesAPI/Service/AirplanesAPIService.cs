3421b3d9c62ce5b5fee429fc88aade93a2c1948a
Giulia Balestra <balestrag9@gmail.com>
2024-12-23 15:48:39 +0100

    GB- Utilizzo metodo BuildUrl e HttpGetRequest
    Rimosso metodo DoGetRequest

--- Codice/FlightInfo/AirplanesAPI/Service/AirplanesAPIService.cs
+++ Codice/FlightInfo/AirplanesAPI/Service/AirplanesAPIService.cs
@@ -8,35 +8,6 @@ namespace AirplanesAPI.Service;
 
 public class AirplanesAPIService
 {
-    private static async Task<string> DoGETRequest(int Scelta , string ParametroDiRicerca)
-    {
-        string BaseUrl = "https://api.airplanes.live/v2/";
-
-
-        switch(Scelta)
-        {
-            case 1:
-                BaseUrl = BaseUrl + "callsign/";
-
-                break;
-               
-            case 2:
-                BaseUrl = BaseUrl + "hex/";
-
-                break;
-
-        }
-
-        BaseUrl = BaseUrl+ParametroDiRicerca;
-
-        HttpClient Client = new HttpClient();
-        // Esegue una richiesta GET all'API e ottiene la risposta JSON
-        HttpResponseMessage Response = await Client.GetAsync(BaseUrl);
-        Response.EnsureSuccessStatusCode();
-        string json = await Response.Content.ReadAsStringAsync();
-
-       return json;
-    }
 
     /*
      * Does a http GET request 
@@ -61,7 +32,7 @@ public class AirplanesAPIService
      * Output: Complete url for the request
      * 
      */
-    private static String BuildUrl(int searchType , String searchParameter)
+    private static string BuildUrl(int searchType , string searchParameter)
     {
         string BaseUrl = "https://api.airplanes.live/v2/";
 
@@ -86,9 +57,7 @@ public class AirplanesAPIService
     
     private static APIResponse ParseJSON(string json)
     {
-        APIResponse Response = JsonConvert.DeserializeObject<APIResponse>(json);
-        
-        return Response;
+        return  JsonConvert.DeserializeObject<APIResponse>(json);   
     }
 
     public static Ac FindAircraftByICAOCode(string ICAO)
@@ -98,8 +67,8 @@ public class AirplanesAPIService
          * DoGETRequest when used with option 2 performs a search using the ICAO CODE
          * 
          */
-        string JSONResponse = DoGETRequest(2, ICAO).Result;
-
+        string JSONResponse = HttpGetRequest(BuildUrl(2, ICAO)).Result;
+            
         if (JSONResponse == null || JSONResponse == "") return null; 
 
         // if the json is not null or empty parse it and then convert the Ac list to array
@@ -111,7 +80,7 @@ public class AirplanesAPIService
     }
     public static Ac FindAircraftByCallsign(string Callsign)
     {
-        string JSONResponse = DoGETRequest(1, Callsign).Result;
+        string JSONResponse = HttpGetRequest(BuildUrl(1, Callsign)).Result;
 
         if (JSONResponse == null || JSONResponse == "") return null;
 
